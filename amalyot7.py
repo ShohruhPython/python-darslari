faa9e0bea235dea9e447c2ccae22b5acf0ce3fc3
Shohruh <161935029+ShohruhPython@users.noreply.github.com>
2024-03-12 23:27:16 +0500

    odingi darslarimdan namuna
    men oldin ishlagan dasturlarimdan bir qancha namuna qo'shdim

---
+++ amalyot7.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 17 15:38:12 2024
+
+@author: QuranShoh
+"""
+
+# isimlar=['Elmurod', 'Usmon', 'Jalil']
+# # print(isimlar)
+# print("Do'stim",isimlar[0], """bugun bir gaplashib olaylik. 
+#       Seni mening yana bir yaqin do'stlarim bilan taishtrmoqchiman. Ularning isimlari""" ,isimlar[1], "va" ,isimlar[2], """
+#       men ularni ham yaxshi ko'raman.""")
+
+
+# sonlar=[3, 4, -3, 7, 4.7]
+# sonlar[0]=8
+# sonlar[1]=sonlar[1]+4
+# print(sonlar[0]+sonlar[4])
+# print(sonlar)
+
+# narxlar=[200, 300, 800, 500]
+# narxlar[3]=800
+# narxlar[2]=narxlar[2]*2
+# narxlar[1]=narxlar[2]+narxlar[3]
+# print(narxlar)
+
+
+
+# t_shaxslar=['A.navoyi', 'A.Temur', "M.Ulug'bek", "J.Manguberdi", "Chingisxon", "Z.M.Bobur" ]
+# x_shaxslar=["E.Mosk", "Moxirdev", "Sukurben", "Raaldo", "Messi", "Xabib"]
+# suxbat_1=t_shaxslar.pop(1)
+# suxbat_2=x_shaxslar.pop(0)
+# print("Men " + suxbat_1 +"""ni ko'rishni va kuch sinashib ko'rishni xoxlar edim. 
+#       Xozrgi shaxslardan esa """ +suxbat_2+ """ bilan gaplashib ilim olishni istaman.
+#       va shu kami kampanya raxbari bo'lishni hohlayman""" )
+
+
+# frends=[]
+# frends.append(input("chaqirmoqchi bo'lgan do'stlaringiz sonini kiriting\n>>>"))
+# frends.append(input(">>>"))
+# frends.append(input(">>>"))
+# frends.append(input(">>>"))
+# frends.append(input(">>>"))
+# frends.append(input(">>>"))
+# print(frends)
+# # frends.remove('Jalil')
+# # del frends[0]
+# # print(frends)
+
+
+
+
+# frends=[]
+# frends.append('Usmon')
+# frends.append("Elmurod")
+# frends.append("Said")
+# frends.append("Fayzulla")
+# frends.append("Asliddin")
+# frends.append("Jalil")
+# print(frends)
+# m1=frends.pop(-5), frends.pop(-4), frends.pop(-3), frends.pop(-2)
+# mexmonlar=[]
+# mexmonlar.append(m1)
+# # mexmonlar.append(m2)
+# print(mexmonlar)
+
+
+# cars=[]
+
+# cars.append(input("moshinalar kiriting\n>>>").title())
+# cars.append(input(">>>").title())
+# cars.append(input(">>>").title())
+# print(cars)
+
+
+
---
+++ python_string_006.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 28 22:03:23 2023
+
+@author: ShohQur
+"""
+
+
+
+a=20
+b=5.5
+temp=36.6
+print(type(a))
+
+aholi_soni=7_594_376_567
+print("axoli soni " ,aholi_soni)
+ 
+odam="har doyim yashash uchun kurashuvchi mavjudod, 35 yoshda qinaladi yeyish uchun harakat qiladi, ishlaydi. duoda bo'ladi bazilari"
+print("odam shundayki " ,odam)
+x, y, z=10, 5.5, -56
+c = a * b
+
+d=100//2
+
+radius=20
+PI=3.14159
+diametr=2*radius
+print("aylana uzunligi" , diametr*PI)
+
+
+ism="davron"
+yosh=23
+yosh=str(yosh)
+xabar=ism.title()+' '+yosh+' yoshda'
+print(xabar)
+
+
+
+t_yil=int(input("Xurmatli mijoz tug'ilgan yilingizni kiriting\n>>>"))
+yosh=2023-t_yil
+print("demak sizni yshingiz" +str(yosh)+ "da")
